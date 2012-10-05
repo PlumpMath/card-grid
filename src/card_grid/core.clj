4c28fcb25781ad273ace485838c064866677d47b
Erik Svedäng <erik.svedang@gmail.com>
2012-10-05 12:31:08 +0200

    Can render text in cards

--- src/card_grid/core.clj
+++ src/card_grid/core.clj
@@ -2,24 +2,41 @@
   (:use [quil.core])
   (:gen-class))
 
+(def grid-width 4)
+
+(def grid-height 5)
+
 (def content
   [{:title "Card one"}
    {:title "Card two"}])
 
+(defn get-content [content x y]
+  (let [i (+ x (* y grid-height))]
+    (get content i)))
+
 (defn setup [])
 
-(defn grid [x-steps y-steps]
+(defn render-card [x y w h]
   (no-fill)
+  (rect x y w h)
+  (let [text-x (+ x 10)
+    	text-y (+ y 20)]
+    (fill 0)
+    (text "HEJ!" text-x text-y)))
+
+(defn grid [x-steps y-steps]
   (let [m 10
         w (/ (- (width) (* m 2)) x-steps)
         h (/ (- (height) (* m 2)) y-steps)]
 	  (doseq [x (range 0 x-steps)
               y (range 0 y-steps)]
-    	(rect (+ m (* x w)) (+ m (* y h)) w h))))
+        (let [rect-x (+ m (* x w))
+              rect-y (+ m (* y h))]
+	        (render-card rect-x rect-y w h)))))
 
 (defn draw []
   (background 240 240 230)
-  (grid 5 6)
+  (grid grid-width grid-height)
   (line 10 300 10 500))
 
 (defn start []
